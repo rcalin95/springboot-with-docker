85723c4403f185eaa60ef82ddfd2cc668d80e513
Calin Razvan <rcalin95@gmail.com>
2023-04-07 22:14:44 +0300

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Docker Demo - Razvan-Costin Calin!";
+        return "Docker Demo - Jimmy!";
     }
 }
